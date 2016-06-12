5cd3b506530338afac79eb673f031662d16066f3
Lee Ourand <lee@leeourand.com>
2016-06-12 16:52:35 -0400

    Allows users to annotate videos, and plays annotations back sync'd with the video

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,15 +12,10 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
-import Player from "./player"
+import socket from "./socket"
+import Video from "./video"
 
-let video = document.getElementById("video")
-
-if(video) {
-  Player.init(video.id, video.getAttribute("data-player-id"), () => {
-    console.log("player ready!")
-  })
-}
+Video.init(socket, document.getElementById("video"))
 // Import local files
 //
 // Local files can be imported directly using relative
